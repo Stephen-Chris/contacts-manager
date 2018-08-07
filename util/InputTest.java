ca60463c593f8b47fa16238c539541cc9c5abf6b
Christopher Mosqueda <christophermosqueda@Christophers-MacBook-Pro.local>
2018-08-07 13:55:03 -0500

    Added util Class

---
+++ util/InputTest.java
@@ -0,0 +1,18 @@
+package util;
+
+//import static util.Input.yesNo;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input newInput = new Input();
+
+        System.out.println(newInput.getString());
+        System.out.println(newInput.getString("\nEnter a value: "));
+        System.out.println(newInput.yesNo());
+        System.out.println(newInput.getInt(5, 20));
+        System.out.println(newInput.getInt());
+        System.out.println(newInput.getInt("\nPick a numnber, any number... "));
+        System.out.println(newInput.getDouble(11.11, 50.2));
+        System.out.println(newInput.getDouble());
+    }
+}
